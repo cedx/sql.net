6071fb55e14bd7589ae0fa15a183814f8bfe9bad
Cédric Belin <cedx@outlook.com>
2025-11-10 10:40:38 +0100

    Add the `ConvertFrom-DataReader` function

---
+++ src/Mapping/ConvertFrom-DataReader.psm1
@@ -0,0 +1,35 @@
+using namespace System.Collections.Generic
+using namespace System.Data
+using module ./ConvertFrom-DataRecord.psm1
+
+<#
+.SYNOPSIS
+	Converts the specified data reader to an array of custom objects or hash tables.
+.PARAMETER InputObject
+	The data reader to convert.
+.PARAMETER AsHashtable
+	Value indicating whether to convert the data reader to an array of hash tables.
+.INPUTS
+	The data reader to convert.
+.OUTPUTS
+	[ordered[]] The array of hash tables corresponding to the specified data reader.
+.OUTPUTS
+	[psobject[]] The array of custom objects corresponding to the specified data reader.
+#>
+function ConvertFrom-DataReader {
+	[OutputType([ordered[]], [psobject[]])]
+	param (
+		[Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+		[IDataReader] $InputObject,
+
+		[Parameter()]
+		[switch] $AsHashtable
+	)
+
+	process {
+		$list = [List[object]]::new()
+		while ($InputObject.Read()) { $list.Add((ConvertFrom-DataRecord $InputObject -AsHashtable:$AsHashtable)) }
+		$InputObject.Close()
+		$list.ToArray()
+	}
+}
