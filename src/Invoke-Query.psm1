c7c2624627c2a7f81ee7f50397d8e694bb98f891
Cédric Belin <cedx@outlook.com>
2025-11-10 11:16:08 +0100

    Add the `Invoke-Query` cmdlet

---
+++ src/Invoke-Query.psm1
@@ -0,0 +1,41 @@
+using namespace System.Data
+using module ./Invoke-Reader.psm1
+using module ./Mapping/ConvertFrom-DataReader.psm1
+
+<#
+.SYNOPSIS
+	Executes a parameterized SQL query and returns a sequence of objects whose properties correspond to the columns.
+.PARAMETER Connection
+	The connection to the data source.
+.PARAMETER Command
+	The SQL query to be executed.
+.PARAMETER Parameters
+	The parameters of the SQL query.
+.PARAMETER AsHashtable
+	Value indicating whether to convert the rows to a hash table.
+.OUTPUTS
+	[hashtable[]] The sequence of hash tables whose keys correspond to the returned columns.
+.OUTPUTS
+	[psobject[]] The sequence of custom objects whose properties correspond to the returned columns.
+#>
+function Invoke-Query {
+	[CmdletBinding()]
+	[OutputType([hashtable[]], [psobject[]])]
+	param (
+		[Parameter(Mandatory, Position = 0)]
+		[IDbConnection] $Connection,
+
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Command,
+
+		[Parameter(Position = 2)]
+		[ValidateNotNull()]
+		[hashtable] $Parameters = @{},
+
+		[Parameter()]
+		[switch] $AsHashtable
+	)
+
+	$reader = Invoke-Reader $Connection -Command $Command -Parameters $Parameters -AsHashtable:$AsHashtable
+	ConvertFrom-DataReader $reader
+}
