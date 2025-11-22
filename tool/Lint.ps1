86039e2b6117fe4d15569b149ec74f86a5cca75a
Cédric Belin <cedx@outlook.com>
2025-11-22 23:38:51 +0100

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,5 +1,4 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
-Invoke-ScriptAnalyzer *.psd1
 Invoke-ScriptAnalyzer src -Recurse
