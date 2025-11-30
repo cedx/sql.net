48a0e7eb449e89632c04c072d10988b0b6399886
Cédric Belin <cedx@outlook.com>
2025-11-30 10:16:01 +0100

    Port the `Get-Version` cmdlet

---
+++ src/Get-Version.cs
@@ -0,0 +1,43 @@
+namespace Belin.Sql.Cmdlets;
+
+using System.Data;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Returns the version of the server associated with the specified connection.
+/// </summary>
+[Cmdlet(VerbsCommon.Get, "Version")]
+[OutputType(typeof(Version))]
+public partial class GetVersion: Cmdlet {
+
+	/// <summary>
+	/// Gets the regular expression used to parse the server version.
+	/// </summary>
+	/// <returns>The regular expression used to parse the server version.</returns>
+	[GeneratedRegex(@"^\d+(\.\d+)+")]
+	private static partial Regex VersionPattern();
+
+	/// <summary>
+	/// The connection to the data source.
+	/// </summary>
+	[Parameter(Mandatory = true, Position = 0)]
+	public required IDbConnection Connection { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() {
+		if (Connection is DbConnection connection) {
+			var match = VersionPattern().Match(connection.ServerVersion);
+			if (match.Success) {
+				WriteObject(Version.Parse(match.Value));
+				return;
+			}
+		}
+
+		var exception = new InvalidOperationException("The server version could not be determined.");
+		WriteError(new ErrorRecord(exception, "UnknownVersion", ErrorCategory.InvalidOperation, null));
+		WriteObject(null);
+	}
+}
