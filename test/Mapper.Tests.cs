03a748acb38be025ef7268b2204b97e24c0b710f
Cédric Belin <cedx@outlook.com>
2025-12-14 10:05:12 +0100

    Update the tests

--- test/Mapper.Tests.cs
+++ test/Mapper.Tests.cs
@@ -7,49 +7,49 @@ namespace Belin.Sql;
 public sealed class MapperTests {
 
 	[DataRow(null, typeof(bool), false, false)]
-	[DataRow(null, typeof(bool?), false, null)]
+	[DataRow(null, typeof(bool?), true, null)]
 	[DataRow(0, typeof(bool), false, false)]
-	[DataRow(0, typeof(bool?), false, false)]
+	[DataRow(0, typeof(bool?), true, false)]
 	[DataRow(1, typeof(bool), false, true)]
-	[DataRow(1, typeof(bool?), false, true)]
+	[DataRow(1, typeof(bool?), true, true)]
 	[DataRow("false", typeof(bool), false, false)]
 	[DataRow("true", typeof(bool), false, true)]
 
 	[DataRow(null, typeof(char), false, char.MinValue)]
-	[DataRow(null, typeof(char?), false, null)]
+	[DataRow(null, typeof(char?), true, null)]
 	[DataRow(0, typeof(char), false, char.MinValue)]
-	[DataRow(0, typeof(char?), false, char.MinValue)]
+	[DataRow(0, typeof(char?), true, char.MinValue)]
 	[DataRow(97, typeof(char), false, 'a')]
 	[DataRow("a", typeof(char), false, 'a')]
 
 	[DataRow(null, typeof(double), false, 0.0)]
-	[DataRow(null, typeof(double?), false, null)]
+	[DataRow(null, typeof(double?), true, null)]
 	[DataRow(0, typeof(double), false, 0.0)]
-	[DataRow(0, typeof(double?), false, 0.0)]
+	[DataRow(0, typeof(double?), true, 0.0)]
 	[DataRow(123, typeof(double), false, 123.0)]
-	[DataRow(-123.456, typeof(double?), false, -123.456)]
+	[DataRow(-123.456, typeof(double?), true, -123.456)]
 	[DataRow("123", typeof(double), false, 123.0)]
 	[DataRow("-123.456", typeof(double), false, -123.456)]
 
 	[DataRow(null, typeof(int), false, 0)]
-	[DataRow(null, typeof(int?), false, null)]
+	[DataRow(null, typeof(int?), true, null)]
 	[DataRow(0, typeof(int), false, 0)]
-	[DataRow(0, typeof(int?), false, 0)]
+	[DataRow(0, typeof(int?), true, 0)]
 	[DataRow(123, typeof(int), false, 123)]
-	[DataRow(-123.456, typeof(int?), false, -123)]
+	[DataRow(-123.456, typeof(int?), true, -123)]
 	[DataRow("123", typeof(int), false, 123)]
 	[DataRow("-123", typeof(int), false, -123)]
 
 	[DataRow(null, typeof(DayOfWeek), false, DayOfWeek.Sunday)]
-	[DataRow(null, typeof(DayOfWeek?), false, null)]
+	[DataRow(null, typeof(DayOfWeek?), true, null)]
 	[DataRow(0, typeof(DayOfWeek), false, DayOfWeek.Sunday)]
-	[DataRow(0, typeof(DayOfWeek?), false, DayOfWeek.Sunday)]
+	[DataRow(0, typeof(DayOfWeek?), true, DayOfWeek.Sunday)]
 	[DataRow(5, typeof(DayOfWeek), false, DayOfWeek.Friday)]
-	[DataRow(5, typeof(DayOfWeek?), false, DayOfWeek.Friday)]
+	[DataRow(5, typeof(DayOfWeek?), true, DayOfWeek.Friday)]
 	[DataRow("sunday", typeof(DayOfWeek), false, DayOfWeek.Sunday)]
 	[DataRow("friday", typeof(DayOfWeek), false, DayOfWeek.Friday)]
 
 	[TestMethod]
-	public void ChangeType(object? value, Type conversionType, bool isNullableReferenceType, object? expected) =>
-		AreEqual(expected, new Mapper().ChangeType(value, conversionType, isNullableReferenceType));
+	public void ChangeType(object? value, Type conversionType, bool isNullable, object? expected) =>
+		AreEqual(expected, new Mapper().ChangeType(value, conversionType, isNullable));
 }
