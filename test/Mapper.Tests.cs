c110bc775fe4feaeb1693a319531ba482b8bcc64
Cédric Belin <cedx@outlook.com>
2025-12-16 16:15:20 +0100

    Update the tests

--- test/Mapper.Tests.cs
+++ test/Mapper.Tests.cs
@@ -28,6 +28,24 @@ public sealed class MapperTests {
 		["a", typeof(char), false, 'a'],
 		["b", typeof(char?), true, 'b'],
 
+		[null, typeof(DateTime), false, DateTime.MinValue],
+		[null, typeof(DateTime?), true, null],
+		[DateTime.MaxValue, typeof(DateTime), false, DateTime.MaxValue],
+		[DateTime.UnixEpoch, typeof(DateTime?), true, DateTime.UnixEpoch],
+		[new DateTime(2025, 6, 7, 10, 45, 1), typeof(DateTime), false, new DateTime(2025, 6, 7, 10, 45, 1)],
+		[new DateTime(2026, 1, 31), typeof(DateTime?), true, new DateTime(2026, 1, 31)],
+		["2025-06-07 10:45:01", typeof(DateTime), false, new DateTime(2025, 6, 7, 10, 45, 1)],
+		["2025-06-07T10:45:01", typeof(DateTime?), true, new DateTime(2025, 6, 7, 10, 45, 1)],
+
+		[null, typeof(DayOfWeek), false, DayOfWeek.Sunday],
+		[null, typeof(DayOfWeek?), true, null],
+		[0, typeof(DayOfWeek), false, DayOfWeek.Sunday],
+		[1, typeof(DayOfWeek?), true, DayOfWeek.Monday],
+		[5, typeof(DayOfWeek), false, DayOfWeek.Friday],
+		[6, typeof(DayOfWeek?), true, DayOfWeek.Saturday],
+		["sunday", typeof(DayOfWeek), false, DayOfWeek.Sunday],
+		["friday", typeof(DayOfWeek?), true, DayOfWeek.Friday],
+
 		[null, typeof(double), false, 0.0],
 		[null, typeof(double?), true, null],
 		[0, typeof(double), false, 0.0],
@@ -37,15 +55,6 @@ public sealed class MapperTests {
 		["123", typeof(double), false, 123.0],
 		["-123.456", typeof(double?), true, -123.456],
 
-		[null, typeof(DateTime), false, DateTime.MinValue],
-		[null, typeof(DateTime?), true, null],
-		[DateTime.MinValue, typeof(DateTime), false, DateTime.MinValue],
-		[DateTime.MaxValue, typeof(DateTime?), true, DateTime.MaxValue],
-		[new DateTime(2025, 6, 7, 10, 45, 1), typeof(DateTime), false, new DateTime(2025, 6, 7, 10, 45, 1)],
-		[new DateTime(2025, 6, 7, 10, 45, 1), typeof(DateTime?), true, new DateTime(2025, 6, 7, 10, 45, 1)],
-		["2025-06-07 10:45:01", typeof(DateTime), false, new DateTime(2025, 6, 7, 10, 45, 1)],
-		["2025-06-07T10:45:01", typeof(DateTime?), true, new DateTime(2025, 6, 7, 10, 45, 1)],
-
 		[null, typeof(int), false, 0],
 		[null, typeof(int?), true, null],
 		[0, typeof(int), false, 0],
@@ -53,16 +62,7 @@ public sealed class MapperTests {
 		[123, typeof(int), false, 123],
 		[-123.456, typeof(int?), true, -123],
 		["123", typeof(int), false, 123],
-		["-123", typeof(int?), true, -123],
-
-		[null, typeof(DayOfWeek), false, DayOfWeek.Sunday],
-		[null, typeof(DayOfWeek?), true, null],
-		[0, typeof(DayOfWeek), false, DayOfWeek.Sunday],
-		[1, typeof(DayOfWeek?), true, DayOfWeek.Monday],
-		[5, typeof(DayOfWeek), false, DayOfWeek.Friday],
-		[6, typeof(DayOfWeek?), true, DayOfWeek.Saturday],
-		["sunday", typeof(DayOfWeek), false, DayOfWeek.Sunday],
-		["friday", typeof(DayOfWeek?), true, DayOfWeek.Friday]
+		["-123", typeof(int?), true, -123]
 	];
 
 	[TestMethod, DynamicData(nameof(TestData))]
