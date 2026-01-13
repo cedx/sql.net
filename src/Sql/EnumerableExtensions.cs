67f1233e9c821750ebb0b2855c1657e8b40a2f23
Cédric Belin <cedx@outlook.com>
2026-01-13 19:55:19 +0100

    Added the `System.Collections.Generic.IEnumerable<T>.AsList()` extension method

---
+++ src/Sql/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+namespace Belin.Sql;
+
+/// <summary>
+/// Provides extension members for enumerables.
+/// </summary>
+public static class EnumerableExtensions {
+	// TODO (.NET 10) extension(IEnumerable<T> enumerable)
+
+	/// <summary>
+	/// Creates a list from the specified enumerable.
+	/// If it is already a list, the enumerable is returned without any duplication.
+	/// </summary>
+	/// <typeparam name="T">The type of enumerable elements.</typeparam>
+	/// <returns>A list that contains elements from the input sequence.</returns>
+	public static List<T> AsList<T>(this IEnumerable<T> enumerable) => enumerable switch {
+    List<T> list => list,
+    _ => Enumerable.ToList(enumerable)
+  };
+}
