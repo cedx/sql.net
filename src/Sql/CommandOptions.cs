2e674b3c109eeec8b74dd5a2923acc5139e49350
Cédric Belin <cedx@outlook.com>
2026-01-13 19:39:32 +0100

    Transform the `CommandOptions` constructor into a parameterless constructor

--- src/Sql/CommandOptions.cs
+++ src/Sql/CommandOptions.cs
@@ -5,11 +5,20 @@ using System.Data;
 /// <summary>
 /// Defines the options of a SQL command.
 /// </summary>
-/// <param name="Timeout">The wait time, in seconds, before terminating the attempt to execute the command and generating an error.</param>
-/// <param name="Transaction">The transaction within which the command executes.</param>
-/// <param name="Type">Value indicating how the command is interpreted.</param>
-public sealed record CommandOptions(
-	int Timeout = 30,
-	IDbTransaction? Transaction = null,
-	CommandType Type = CommandType.Text
-);
+public record CommandOptions {
+
+	/// <summary>
+	/// The wait time, in seconds, before terminating the attempt to execute the command and generating an error.
+	/// </summary>
+	public int Timeout { get; init; } = 30;
+
+	/// <summary>
+	/// The transaction within which the command executes.
+	/// </summary>
+	public IDbTransaction? Transaction { get; init; }
+
+	/// <summary>
+	/// Value indicating how the command is interpreted.
+	/// </summary>
+	public CommandType Type { get; init; } = CommandType.Text;
+}
