69ae11d19138368635c7b3a4ba93fc85614ae3d2
Cédric Belin <cedx@outlook.com>
2026-01-20 21:20:14 +0100

    Code formatting [skip ci]

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,4 +1,5 @@
 "Deleting all generated files..."
 Remove-Item bin -ErrorAction Ignore -Force -Recurse
-Remove-Item */obj -Force -Recurse
+Remove-Item src/*/obj -Force -Recurse
+Remove-Item test/obj -ErrorAction Ignore -Force -Recurse
 Remove-Item var/* -Exclude .gitkeep -Force -Recurse
