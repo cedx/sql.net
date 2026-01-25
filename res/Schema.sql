3294537ab80a370d1fcbb927e616a1a3793b3940
Cédric Belin <cedx@outlook.com>
2026-01-25 19:50:09 +0100

    Add the test database [skip ci]

---
+++ res/Schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS "Characters";
+CREATE TABLE "Characters" (
+	"Id" integer PRIMARY KEY,
+	"FirstName" text NOT NULL CHECK (LENGTH("FirstName") > 0),
+	"LastName" text,
+	"Gender" text NOT NULL CHECK ("Gender" IN ('Balrog', 'Dark Lord', 'Dwarf', 'Elf', 'Hobbit', 'Human', 'Istari'))
+);
+
+CREATE INDEX "IX_Characters_Gender" ON "Characters" ("Gender");
+INSERT INTO "Characters" ("FirstName", "LastName", "Gender") VALUES
+	('Aragorn', NULL, 'Human'),
+	('Balin', NULL, 'Dwarf'),
+	('Boromir', NULL, 'Human'),
+	('Durin''s Bane', NULL, 'Balrog'),
+	('Elrond', NULL, 'Elf'),
+	('Frodo', 'Baggins', 'Hobbit'),
+	('Galadriel', NULL, 'Elf'),
+	('Gandalf', NULL, 'Istari'),
+	('Gimli', NULL, 'Dwarf'),
+	('Gollum', NULL, 'Hobbit'),
+	('Gothmog', NULL, 'Balrog'),
+	('Legolas', NULL, 'Elf'),
+	('Pippin', 'Took', 'Hobbit'),
+	('Sam', 'Gamgee', 'Hobbit'),
+	('Saruman', NULL, 'Istari'),
+	('Sauron', NULL, 'Dark Lord');
