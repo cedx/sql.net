4824fbc5c747ac40c43317cdd0f97edeeaefe673
Cédric Belin <cedx@outlook.com>
2026-01-28 17:59:13 +0100

    Add the `Character` fixture [skip ci]

---
+++ test/Fixtures/Character.cs
@@ -0,0 +1,22 @@
+namespace Belin.Sql.Fixtures;
+
+/// <summary>
+/// Represents a fictional character from a well-known saga.
+/// </summary>
+public sealed class Character {
+
+	/// <summary>
+	/// The first name.
+	/// </summary>
+	public string FirstName { get; set; } = "";
+
+	/// <summary>
+	/// The character's gender.
+	/// </summary>
+	public CharacterGender Gender { get; set; } = CharacterGender.Human;
+
+	/// <summary>
+	/// The last name.
+	/// </summary>
+	public string LastName { get; set; } = "";
+}
---
+++ test/Fixtures/CharacterGender.cs
@@ -0,0 +1,14 @@
+namespace Belin.Sql.Fixtures;
+
+/// <summary>
+/// Defines the gender of a character.
+/// </summary>
+public enum CharacterGender {
+	Balrog,
+	DarkLord,
+	Dwarf,
+	Elf,
+	Hobbit,
+	Human,
+	Istari
+}
