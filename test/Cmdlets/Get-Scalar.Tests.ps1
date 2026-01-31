00b5773ecb05183415c5de8b81b5437fb46c98c8
Cédric Belin <cedx@outlook.com>
2026-01-31 03:17:55 +0100

    Update the dependencies

---
+++ test/Cmdlets/Get-Scalar.Tests.ps1
@@ -0,0 +1,27 @@
+using namespace System.Diagnostics.CodeAnalysis
+
+<#
+.SYNOPSIS
+	Tests the features of the `Get-Scalar` cmdlet.
+#>
+Describe "Get-Scalar" {
+	BeforeAll {
+		Import-Module "$PSScriptRoot/../../Sql.psd1"
+		Import-Module "$PSScriptRoot/../../bin/System.Data.SQLite.dll"
+	}
+
+	BeforeEach {
+		[SuppressMessage("PSUseDeclaredVarsMoreThanAssignments", "")]
+		$connection = [System.Data.SQLite.SQLiteConnection]::new("DataSource=$PSScriptRoot/../../res/Database.sqlite")
+	}
+
+	AfterEach {
+		$connection.Close()
+	}
+
+	It "should return the single value produced by the SQL query" {
+		$sql = "SELECT COUNT(*) FROM Characters WHERE Gender = @Gender"
+		$value = Get-SqlScalar $connection -Command $sql -Parameters @{ Gender = "Balrog" }
+		$value | Should -Be 2
+	}
+}
---
+++ test/ConnectionExtensions.Tests.cs
@@ -0,0 +1,65 @@
+namespace Belin.Sql;
+
+using Belin.Sql.Fixtures;
+using System.Data.SQLite;
+
+/// <summary>
+/// Tests the features of the <see cref="ConnectionExtensions"/> class.
+/// </summary>
+/// <param name="testContext">The test context.</param>
+[TestClass]
+public sealed class ConnectionExtensionsTests(TestContext testContext) {
+
+	/// <summary>
+	/// The connection to the data source.
+	/// </summary>
+	private SQLiteConnection connection = default!;
+
+	[TestInitialize]
+	public void TestInitialize() => connection = new SQLiteConnection($"DataSource={Path.Join(AppContext.BaseDirectory, "../res/Database.sqlite")}");
+
+	[TestCleanup]
+	public void TestCleanup() => connection.Close();
+
+	[TestMethod]
+	public async Task ExecuteScalar() {
+		var sql = "SELECT COUNT(*) FROM Characters WHERE Gender = @Gender";
+		var parameters = new ParameterCollection("Gender", CharacterGender.Balrog.ToString());
+		AreEqual(2, connection.ExecuteScalar<int>(sql, parameters));
+		AreEqual(2, await connection.ExecuteScalarAsync<int>(sql, parameters, cancellationToken: testContext.CancellationToken));
+	}
+
+	//[TestMethod]
+	//public async Task Query() {
+	//	var sql = "SELECT * FROM Characters WHERE Gender = @Gender ORDER BY FullName";
+	//	var parameters = new ParameterCollection("Gender", CharacterGender.Elf.ToString());
+
+	//	var records = connection.Query<Character>(sql, parameters).AsList();
+	//	HasCount(3, records);
+
+	//	records = (await connection.QueryAsync<Character>(sql, parameters, cancellationToken: testContext.CancellationToken)).AsList();
+	//	HasCount(3, records);
+	//}
+
+	//[TestMethod]
+	//public async Task QueryFirst() {
+	//	var sql = "SELECT * FROM Characters WHERE FullName = @FullName";
+	//	var parameters = new ParameterCollection("FullName", "Sauron");
+
+	//	var record = connection.QueryFirst<Character>(sql, parameters);
+	//	AreEqual("Sauron", record.FirstName);
+	//	AreEqual(CharacterGender.DarkLord, record.Gender);
+	//}
+
+	//[TestMethod]
+	//public async Task QuerySingle() {
+	//	var sql = "SELECT * FROM Characters WHERE Gender = @Gender ORDER BY FullName";
+	//	var parameters = new ParameterCollection("Gender", CharacterGender.Elf.ToString());
+
+	//	var records = connection.Query<Character>(sql, parameters).AsList();
+	//	HasCount(3, records);
+
+	//	records = (await connection.QueryAsync<Character>(sql, parameters, cancellationToken: testContext.CancellationToken)).AsList();
+	//	HasCount(3, records);
+	//}
+}
