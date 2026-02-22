cd8be677bbf7c2ea3a3e5a7ac153880bd9a882d8
Cédric Belin <cedx@outlook.com>
2026-02-22 14:22:59 +0100

    Update the "Lint" script [skip ci]

--- test/Cmdlets/Get-Mapper.Tests.ps1
+++ test/Cmdlets/Get-Mapper.Tests.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
 	Tests the features of the `Get-Mapper` cmdlet.
 #>
--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,4 +1,5 @@
 "Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
+Invoke-ScriptAnalyzer test -Recurse
 Test-ModuleManifest Sql.psd1 | Out-Null
