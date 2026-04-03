7a283429bcdb98fbd9a6fea49ae1c4c8cdf418d1
Cédric Belin <cedx@outlook.com>
2026-04-03 19:28:43 +0200

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -15,11 +15,12 @@ git push origin "v$version"
 
 $output = "var/NuGet"
 dotnet pack --output $output
+Remove-Item "$output/*.Cmdlets.*"
 Get-Item "$output/*.nupkg" | ForEach-Object { dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source NuGet }
 
 $output = "var/PSModule"
 New-Item $output/bin -ItemType Directory | Out-Null
-Copy-Item Sql.psd1 $output
+Copy-Item Sql.psd1 $output/Belin.Sql.psd1
 Copy-Item *.md $output
 Copy-Item $module.RootModule $output/bin
 $module.RequiredAssemblies | Copy-Item -Destination $output/bin
@@ -27,4 +28,4 @@ $module.RequiredAssemblies | Copy-Item -Destination $output/bin
 $output = "var/PSGallery"
 New-Item $output -ItemType Directory | Out-Null
 Compress-PSResource var/PSModule $output
-Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ }
+Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ -Repository PSGallery }
