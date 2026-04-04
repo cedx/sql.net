a5bbf85635960508d9b8ca244d8921502f16b3d3
Cédric Belin <cedx@outlook.com>
2026-04-04 13:22:26 +0200

    Update the build system

--- tool/Outdated.ps1
+++ tool/Outdated.ps1
@@ -1,2 +1,5 @@
+using module ./PSResource.psm1
+
 "Checking for outdated dependencies..."
 dotnet package list --outdated
+(Import-PowerShellDataFile PSModules.psd1).Keys | Get-InstalledPSResource | Test-PSResourceUpdate
--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -15,13 +15,12 @@ git push origin "v$version"
 
 $output = "var/NuGet"
 dotnet pack --output $output
-Get-Item "$output/*.nupkg" | ForEach-Object {
-	dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
-}
+Remove-Item "$output/*.Cmdlets.*"
+Get-Item "$output/*.nupkg" | ForEach-Object { dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source NuGet }
 
 $output = "var/PSModule"
 New-Item $output/bin -ItemType Directory | Out-Null
-Copy-Item Sql.psd1 $output
+Copy-Item Sql.psd1 $output/Belin.Sql.psd1
 Copy-Item *.md $output
 Copy-Item $module.RootModule $output/bin
 $module.RequiredAssemblies | Copy-Item -Destination $output/bin
@@ -29,4 +28,4 @@ $module.RequiredAssemblies | Copy-Item -Destination $output/bin
 $output = "var/PSGallery"
 New-Item $output -ItemType Directory | Out-Null
 Compress-PSResource var/PSModule $output
-Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ }
+Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ -Repository PSGallery }
