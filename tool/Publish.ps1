33e416ee40174d0fe9bed6a8f8b54b19677ef5d7
Cédric Belin <cedx@outlook.com>
2026-04-08 22:19:29 +0200

    Update the "Publish" script

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -15,14 +15,14 @@ git push origin "v$version"
 
 $output = "var/NuGet"
 dotnet pack --output $output
-Remove-Item "$output/*.Cmdlets.*"
 Get-Item "$output/*.nupkg" | ForEach-Object { dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source NuGet }
 
 $output = "var/PSModule"
 New-Item $output/bin -ItemType Directory | Out-Null
 Copy-Item Sql.psd1 $output/Belin.Sql.psd1
 Copy-Item *.md $output
-Copy-Item $module.RootModule $output/bin
+Copy-Item src $output -Recurse
+Remove-Item $output/src/*.cs*, $output/src/obj -Recurse
 $module.RequiredAssemblies | Copy-Item -Destination $output/bin
 
 $output = "var/PSGallery"
