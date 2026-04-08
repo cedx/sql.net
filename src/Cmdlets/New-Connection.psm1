a0c6136b9e7a04ff67093232efb4731d72770449
Cédric Belin <cedx@outlook.com>
2026-04-08 22:33:09 +0200

    Port the connection cmdlets

---
+++ src/Cmdlets/New-Connection.psm1
@@ -0,0 +1,32 @@
+using namespace System.Data
+
+<#
+.SYNOPSIS
+	Creates a new database connection.
+.INPUTS
+	The connection string used to open the database.
+.OUTPUTS
+	The newly created database connection.
+#>
+function New-Connection {
+	[CmdletBinding()]
+	[OutputType([System.Data.IDbConnection])]
+	param (
+		# The type of connection class to instantiate.
+		[Parameter(Mandatory, Position = 0)]
+		[Type] $Type,
+
+		# The connection string used to open the database.
+		[Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+		[string] $ConnectionString,
+
+		# Value indicating whether to open the connection.
+		[switch] $Open
+	)
+
+	process {
+		$connection = [IDbConnection] [Activator]::CreateInstance($Type, $ConnectionString)
+		if ($Open) { $connection.Open() }
+		$connection
+	}
+}
