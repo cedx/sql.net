3deb008c3fc363d3702d970d81731b6cbdf27377
Cédric Belin <cedx@outlook.com>
2026-04-09 21:48:52 +0200

    Port the `Invoke-Reader` cmdlet

---
+++ src/Cmdlets/Invoke-Reader.psm1
@@ -0,0 +1,39 @@
+using namespace Belin.Sql
+using namespace System.Data
+
+<#
+.SYNOPSIS
+	Executes a parameterized SQL query and returns a data reader.
+.OUTPUTS
+	The data reader that can be used to access the results.
+#>
+function Invoke-Reader {
+	[CmdletBinding()]
+	[OutputType([System.Data.IDataReader])]
+	param (
+		# The connection to the data source.
+		[Parameter(Mandatory, Position = 0)]
+		[IDbConnection] $Connection,
+
+		# The SQL query to be executed.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Command,
+
+		# The parameters of the SQL query.
+		[Parameter(Position = 2)]
+		[ParameterCollection] $Parameters = @(),
+
+		# Value indicating how the command is interpreted.
+		[CommandType] $CommandType = [CommandType]::Text,
+
+		# The wait time, in seconds, before terminating the attempt to execute the command and generating an error.
+		[ValidateRange("Positive")]
+		[int] $Timeout = 30,
+
+		# The transaction to use, if any.
+		[IDbTransaction] $Transaction
+	)
+
+	$reader = $Connection.ExecuteReader($Command, $Parameters, [CommandOptions]@{ Timeout = $Timeout; Transaction = $Transaction; Type = $CommandType })
+	Write-Output $reader -NoEnumerate
+}
