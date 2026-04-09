d12b84b926ddd7575861ab40102ef1bb94885d1f
Cédric Belin <cedx@outlook.com>
2026-04-09 20:00:19 +0200

    Port the `Get-Scalar` cmdlet

---
+++ src/Cmdlets/Get-Scalar.psm1
@@ -0,0 +1,52 @@
+using namespace Belin.Sql
+using namespace System.Data
+using namespace System.Reflection
+
+<#
+.SYNOPSIS
+	An array of types representing the number, order, and type of the parameters of the underlying method to invoke.
+#>
+$ParameterTypes = [Type[]] [IDbConnection], [string], [ParameterCollection], [CommandOptions]
+
+<#
+.SYNOPSIS
+	Executes a parameterized SQL query that selects a single value.
+.OUTPUTS
+	The first column of the first row.
+#>
+function Get-Scalar {
+	[CmdletBinding()]
+	[OutputType([object])]
+	param (
+		# The connection to the data source.
+		[Parameter(Mandatory, Position = 0)]
+		[IDbConnection] $Connection,
+
+		# The SQL query to be executed.
+		[Parameter(Mandatory, Position = 1)]
+		[string] $Command,
+
+		# The parameters of the SQL query.
+		[Parameter(Position = 2)]
+		[ParameterCollection] $Parameters = @(),
+
+		# Value indicating how the command is interpreted.
+		[CommandType] $CommandType = [CommandType]::Text,
+
+		# The wait time, in seconds, before terminating the attempt to execute the command and generating an error.
+		[ValidateRange("Positive")]
+		[int] $Timeout = 30,
+
+		# The transaction to use, if any.
+		[IDbTransaction] $Transaction
+	)
+
+	try {
+		$method = [ConnectionExtensions]::GetMethod("ExecuteScalar", 1, $Script:ParameterTypes).MakeGenericMethod([object])
+		$arguments = $Connection, $Command, $Parameters, [CommandOptions]@{ Timeout = $Timeout; Transaction = $Transaction; Type = $CommandType }
+		$method.Invoke($null, $arguments)
+	}
+	catch [TargetInvocationException] {
+		throw [DataException]::new($_.Exception.InnerException.Message, $_.Exception)
+	}
+}
