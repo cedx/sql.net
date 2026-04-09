edd29bd2c69b72018ffab9393956fda96b9e65d3
Cédric Belin <cedx@outlook.com>
2026-04-09 21:53:03 +0200

    Port the `New-Parameter` cmdlet

---
+++ src/Cmdlets/New-Parameter.psm1
@@ -0,0 +1,47 @@
+using namespace Belin.Sql
+using namespace System.Data
+
+<#
+.SYNOPSIS
+	Creates a new command parameter.
+.OUTPUTS
+	The newly created parameter.
+#>
+function New-Parameter {
+	[CmdletBinding()]
+	[OutputType([Belin.Sql.Parameter])]
+	param (
+		# The parameter name.
+		[Parameter(Mandatory, Position = 0)]
+		[AllowEmptyString()]
+		[string] $Name,
+
+		# The parameter value.
+		[Parameter(Position = 1)]
+		[AllowNull()]
+		[object] $Value,
+
+		# Value indicating whether this parameter is input-only, output-only, bidirectional, or a stored procedure return value parameter.
+		[ParameterDirection] $Direction,
+
+		# The database type of this parameter.
+		[DbType] $DbType,
+
+		# The maximum size of this parameter, in bytes.
+		[int] $Size,
+
+		# Indicates the precision of numeric parameters.
+		[byte] $Precision,
+
+		# Indicates the scale of numeric parameters.
+		[byte] $Scale
+	)
+
+	$parameter = [Belin.Sql.Parameter]::new($Name, $Value)
+	$parameter.DbType = DbType
+	$parameter.Direction = Direction
+	$parameter.Precision = Precision
+	$parameter.Scale = Scale
+	$parameter.Size = Size
+	$parameter
+}
