25d82d5b5f50c3c6aa70c3adb5475334f7db0971
Cédric Belin <cedx@outlook.com>
2026-04-14 23:45:52 +0200

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,7 +1,4 @@
-if ($Release) {
-	& "$PSScriptRoot/Clean.ps1"
-	& "$PSScriptRoot/Version.ps1"
-}
+if ($Release) { & "$PSScriptRoot/Default.ps1" }
 else {
 	"The ""-Release"" switch must be set!"
 	exit 1
@@ -14,7 +11,7 @@ git tag "v$version"
 git push origin "v$version"
 
 $output = "var/NuGet"
-dotnet pack --output $output
+dotnet pack --no-build --output $output
 Get-Item "$output/*.nupkg" | ForEach-Object { dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source NuGet }
 
 $output = "var/PSModule"
