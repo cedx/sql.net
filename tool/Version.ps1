b88d0e0f9a7d96a9dfaf3b01deac771a1be1fb77
Cédric Belin <cedx@outlook.com>
2026-04-25 23:43:16 +0200

    Update the "Version" script [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 "Updating the version number in the sources..."
 $version = (Import-PowerShellDataFile Sql.psd1).ModuleVersion
 foreach ($file in Get-ChildItem -Filter *.csproj -Recurse) {
-	(Get-Content $file -Raw) -replace "<Version>\d+(\.\d+){2}.*</Version>", "<Version>$version</Version>" | Set-Content $file -NoNewLine
+	(Get-Content $file -Raw) -replace "<Version>\d+(\.\d+)</Version>", "<Version>$version</Version>" | Set-Content $file -NoNewLine
 }
