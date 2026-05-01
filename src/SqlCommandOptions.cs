7fa731b2c576276f7630ccb201aab3e361dec696
Cédric Belin <cedx@outlook.com>
2026-05-01 23:56:19 +0200

    Code optimization [skip ci]

--- src/SqlCommandOptions.cs
+++ src/SqlCommandOptions.cs
@@ -10,7 +10,7 @@ public record SqlCommandOptions {
 	/// <summary>
 	/// The wait time, in seconds, before terminating the attempt to execute the command and generating an error.
 	/// </summary>
-	public int Timeout { get; init; } = 30;
+	public int? Timeout { get; init; }
 
 	/// <summary>
 	/// The transaction within which the command executes.
@@ -20,5 +20,5 @@ public record SqlCommandOptions {
 	/// <summary>
 	/// Value indicating how the command is interpreted.
 	/// </summary>
-	public CommandType Type { get; init; } = CommandType.Text;
+	public CommandType? Type { get; init; }
 }
