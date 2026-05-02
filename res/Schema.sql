b7bf74dfb56a26abfd0c9ff451b9f508015b61ec
Cédric Belin <cedx@outlook.com>
2026-05-03 00:54:16 +0200

    Update the database schema [skip ci]

--- res/Schema.sql
+++ res/Schema.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS "Skills";
 DROP TABLE IF EXISTS "Characters";
+
 CREATE TABLE "Characters" (
 	"ID" integer PRIMARY KEY,
 	"firstName" text NOT NULL CHECK (LENGTH("firstName") > 0),
@@ -7,8 +9,12 @@ CREATE TABLE "Characters" (
 	"gender" text NOT NULL DEFAULT 'Human' CHECK ("gender" IN ('Balrog', 'DarkLord', 'Dwarf', 'Elf', 'Hobbit', 'Human', 'Istari'))
 );
 
-CREATE INDEX "IX_Characters_FullName" ON "Characters" ("fullName");
-CREATE INDEX "IX_Characters_Gender" ON "Characters" ("gender");
+CREATE TABLE "Skills" (
+	"ID" integer PRIMARY KEY,
+	"characterID" integer REFERENCES "Characters" ("ID") ON UPDATE CASCADE ON DELETE CASCADE,
+	"name" text NOT NULL CHECK (LENGTH("name") > 0),
+	"score" integer NOT NULL CHECK ("score" > 0)
+);
 
 INSERT INTO "Characters" ("firstName", "lastName", "gender") VALUES
 	('Aragorn', NULL, 'Human'),
@@ -27,3 +33,15 @@ INSERT INTO "Characters" ("firstName", "lastName", "gender") VALUES
 	('Sam', 'Gamgee', 'Hobbit'),
 	('Saruman', NULL, 'Istari'),
 	('Sauron', NULL, 'DarkLord');
+
+INSERT INTO "Skills" ("characterID", "name", "score") VALUES
+	(1, "Sword", 90),
+	(2, "Hammer", 80),
+	(3, "Sword", 75),
+	(5, "Bow", 80),
+	(6, "Invisibility", 100),
+	(7, "Beauty", 95),
+	(8, "Magic", 80),
+	(12, "Bow", 95),
+	(15, "Magic", 85),
+	(16, "Magic", 95);
