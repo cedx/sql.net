e5b896aaa9a3726423bde57921992004a2d15727
Cédric Belin <cedx@outlook.com>
2026-05-04 11:42:53 +0200

    Add the `SortOrder` enum [skip ci]

---
+++ src/SortOrder.cs
@@ -0,0 +1,17 @@
+namespace Belin.Sql;
+
+/// <summary>
+/// Specifies how rows of data are sorted.
+/// </summary>
+public enum SortOrder {
+
+	/// <summary>
+	/// Rows are sorted in ascending order.
+	/// </summary>
+	Ascending,
+
+	/// <summary>
+	/// Rows are sorted in descending order.
+	/// </summary>
+	Descending
+}
